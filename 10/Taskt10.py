4415aa094feeab78350a6159ece456cdd1528709
musman61 <151510703+musman61@users.noreply.github.com>
2023-11-27 12:09:08 +0500

    Create Taskt10.py

---
+++ 10/Taskt10.py
@@ -0,0 +1,8 @@
+def concatenate_lists(list1, list2):
+    return list1+list2
+
+
+
+
+combined_list = concatenate_lists([1, 2, 3], [4, 5, 6])
+print(combined_list)  # Should output [1, 2, 3, 4, 5, 6]
